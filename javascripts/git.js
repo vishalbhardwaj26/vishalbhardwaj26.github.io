bfd035687b9acd157a167886cbb4a099832f73b5
Vishal Bhardwaj <vishal.bhardwaj26@gmail.com>
2017-08-10 23:48:52 +0530

    add

--- javascripts/git.js
+++ javascripts/git.js
@@ -8,12 +8,12 @@ jQuery.fn.loadRepositories = function(username) {
   var target = this; 
   $.githubUser(username, function(data) {
     var repos = data;
-    sortByNumberOfWatchers(repos);
+    sortByName(repos);
  
     var list = $('<ul/>');
     target.empty().append(list);
     $(repos).each(function() {
-      if (this.name != (username.toLowerCase()+'.github.com')) {
+      if (this.name != (username.toLowerCase()+'.github.io')) {
        list.append('<li><a href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</a></li>');
         if (this.description != null) {
                 list.append('<dd>' + this.description +'</dd>');
@@ -30,4 +30,9 @@ jQuery.fn.loadRepositories = function(username) {
       return b.watchers - a.watchers;
     });
   }
+   function sortByName(repos) {
+        repos.sort(function(a,b) {
+        return a.name - b.name;
+       });
+    }
 };
