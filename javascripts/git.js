b84d1b24c041f8163b0da61c2468319751bb438b
Vishal Bhardwaj <vishal_bhardwaj875@keysight.com>
2017-08-10 23:25:05 +0530

    added helloworld page

---
+++ javascripts/git.js
@@ -0,0 +1,33 @@
+jQuery.githubUser = function(username, callback) {
+  jQuery.getJSON("https://api.github.com/users/" + username + "/repos", callback);
+}
+ 
+jQuery.fn.loadRepositories = function(username) {
+  this.html("<span>Loading...</span>");
+ 
+  var target = this; 
+  $.githubUser(username, function(data) {
+    var repos = data;
+    sortByNumberOfWatchers(repos);
+ 
+    var list = $('<ul/>');
+    target.empty().append(list);
+    $(repos).each(function() {
+      if (this.name != (username.toLowerCase()+'.github.com')) {
+       list.append('<li><a href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</a></li>');
+        if (this.description != null) {
+                list.append('<dd>' + this.description +'</dd>');
+        }
+              list.append('</br>');
+      }
+      // list.append('<dt><a href="'+ this.url +'">' + this.name + '</a></dt>');
+      // list.append('<dd>' + this.description + '</dd>');
+    });
+  });
+ 
+  function sortByNumberOfWatchers(repos) {
+    repos.sort(function(a,b) {
+      return b.watchers - a.watchers;
+    });
+  }
+};
